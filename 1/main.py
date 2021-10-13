4e730d1237539f347edbba34fc18fb22d34399a3
gibran <gibi.gibran@gmail.com>
2021-10-13 23:31:51 +0700

    my first python code!

---
+++ 1/main.py
@@ -0,0 +1,13 @@
+"""
+semua sintaksis dasa bahasa pemograman terdiri dari:
+1. Sekuensial : langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# sekuensial
+print('ibu berkata,"pergi ke toko"')
+print('Budi menjawab,"Baik, apa yang harus saya lakukan di toko?"')
+print('ibu menjawab,"beli 1 botol susu dan jika ada telor beli 6"')
+print('maka Budi berangkat ke toko')
+print('Dan mulai berbelanja')
